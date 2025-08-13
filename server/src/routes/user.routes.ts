ea1debffcae6436b9158a012e7f8f5b6f54c7a95
njatoty <njatotianafiononana@gmail.com>
2025-08-13 14:10:54 +0300

    jest setup

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { UserController } from '@/controllers/user.controller';
+import { validate } from '@/middlewares/validate';
+import { userCreateSchema } from '@/validators/user.validator';
 
 const router = express.Router();
 const controller = new UserController();
 
-router.post('/', controller.create.bind(controller));
+router.post('/', validate(userCreateSchema), controller.create.bind(controller));
 router.get('/:id', controller.getById.bind(controller));
 router.put('/:id', controller.update.bind(controller));
 router.patch('/:id/deactivate', controller.deactivate.bind(controller));
