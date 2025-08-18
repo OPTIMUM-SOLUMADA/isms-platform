fdc27739c3e664dc40c22b3c0d353bf861fd775b
njatoty <njatotianafiononana@gmail.com>
2025-08-18 10:32:35 +0300

    refactor: add error code

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -19,12 +19,18 @@ export class AuthController {
         try {
             const user = await authService.login(email, password);
             if (!user) {
-                res.status(400).json({ error: 'Invalid email or password' });
+                res.status(400).json({
+                    error: 'Invalid email or password',
+                    code: 'ERR_INVALID_CREDENTIALS'
+                });
                 return;
             }
 
             if (!user.isActive) {
-                res.status(400).json({ error: 'User is inactive. Please contact admin.' });
+                res.status(400).json({
+                    error: 'User is inactive. Please contact admin.',
+                    code: 'ERR_USER_INACTIVE'
+                });
                 return;
             }
 
@@ -45,7 +51,10 @@ export class AuthController {
                 });
         } catch (err) {
             console.error(err);
-            res.status(500).json({ error: (err as Error).message });
+            res.status(500).json({
+                error: (err as Error).message,
+                code: 'ERR_SERVER_ERROR'
+            });
         }
     }
 
@@ -63,7 +72,10 @@ export class AuthController {
             const decoded = jwtService.verifyRefreshToken(refreshToken) as any;
             const user = await userService.findByEmail(decoded.user.email);
             if (!user) {
-                res.status(404).json({ user: 'User not found' });
+                res.status(404).json({
+                    error: 'User not found',
+                    code: 'ERR_USER_NOT_FOUND'
+                });
             } else {
                 const accessToken = jwtService.generateAccessToken(user);
                 const { passwordHash, ...rest } = user;
@@ -74,7 +86,10 @@ export class AuthController {
             }
         } catch (error: any) {
             console.error(error);
-            res.status(400).send('Invalid refresh token.');
+            res.status(400).send({
+                error: 'Invalid refresh token.',
+                code: 'ERR_INVALID_REFRESH_TOKEN'
+            });
         }
     }
 
@@ -91,7 +106,10 @@ export class AuthController {
             const decoded = jwtService.verifyToken(accessToken!);
             const user = await userService.findByEmail(decoded.user.email);
             if (!user) {
-                res.status(404).json({ error: 'User not found' });
+                res.status(404).json({
+                    error: 'User not found',
+                    code: 'ERR_USER_NOT_FOUND'
+                });
                 return;
             }
             res.json(user);
@@ -124,7 +142,10 @@ export class AuthController {
         try {
             const user = await userService.findByEmail(email);
             if (!user) {
-                res.status(404).json({ error: 'User not found' });
+                res.status(404).json({
+                    error: 'User not found',
+                    code: 'ERR_USER_NOT_FOUND'
+                });
                 return;
             }
 
@@ -147,7 +168,10 @@ export class AuthController {
             res.status(200).json({ message: 'Password reset email sent' });
         } catch (err) {
             console.error(err);
-            res.status(500).json({ error: (err as Error).message });
+            res.status(500).json({
+                error: (err as Error).message,
+                code: 'ERR_SERVER_ERROR'
+            });
         }
     }
 
@@ -158,16 +182,21 @@ export class AuthController {
             console.log(resetToken)
             const decoded = await jwtService.verifyPasswordResetToken(resetToken);
 
-            console.log(decoded)
             // find user
             const user = await userService.findByEmail(decoded.user.email);
             if (!user) {
-                res.status(404).json({ error: 'User not found' });
+                res.status(404).json({
+                    error: 'User not found',
+                    code: 'ERR_USER_NOT_FOUND'
+                });
                 return;
             }
 
             if (user.passwordResetToken !== resetToken) {
-                res.status(400).json({ error: 'Invalid or expired token' });
+                res.status(400).json({
+                    error: 'Invalid or expired token',
+                    code: 'ERR_INVALID_TOKEN'
+                });
                 return;
             }
 
@@ -182,11 +211,20 @@ export class AuthController {
         } catch (err) {
             console.error(err);
             if (err instanceof jwt.TokenExpiredError) {
-                res.status(401).json({ error: "Reset token has expired" });
+                res.status(401).json({
+                    error: "Reset token has expired",
+                    code: "ERR_RESET_TOKEN_EXPIRED"
+                });
             } else if (err instanceof jwt.JsonWebTokenError) {
-                res.status(401).json({ error: "Invalid reset token" });
+                res.status(401).json({
+                    error: "Invalid reset token",
+                    code: "ERR_INVALID_RESET_TOKEN"
+                });
             } else {
-                res.status(500).json({ error: (err as Error).message });
+                res.status(500).json({
+                    error: (err as Error).message,
+                    code: "ERR_SERVER_ERROR"
+                });
             }
         }
     }
@@ -200,12 +238,18 @@ export class AuthController {
             // find user
             const user = await userService.findByEmail(decoded.user.email);
             if (!user) {
-                res.status(404).json({ error: 'User not found' });
+                res.status(404).json({
+                    error: 'User not found',
+                    code: 'ERR_USER_NOT_FOUND'
+                });
                 return;
             }
 
             if (user.passwordResetToken !== resetToken) {
-                res.status(400).json({ error: 'Link has expired' });
+                res.status(400).json({
+                    error: 'Link has expired',
+                    code: 'ERR_RESET_TOKEN_EXPIRED'
+                });
                 return;
             }
 
@@ -213,11 +257,20 @@ export class AuthController {
         } catch (err) {
             console.error("Error verifying password reset token:", err);
             if (err instanceof jwt.TokenExpiredError) {
-                res.status(401).json({ error: "Reset token has expired" });
+                res.status(401).json({
+                    error: "Reset token has expired",
+                    code: "ERR_RESET_TOKEN_EXPIRED"
+                });
             } else if (err instanceof jwt.JsonWebTokenError) {
-                res.status(401).json({ error: "Invalid reset token" });
+                res.status(401).json({
+                    error: "Invalid reset token",
+                    code: "ERR_INVALID_RESET_TOKEN"
+                });
             } else {
-                res.status(500).json({ error: "Server error verifying token" });
+                res.status(500).json({
+                    error: "Server error verifying token",
+                    code: "ERR_SERVER_ERROR"
+                });
             }
         }
     }
