982b9ae7eafd20abd08653cf43e3f26b3604ef3b
njatoty <njatotianafiononana@gmail.com>
2025-10-15 15:51:31 +0300

    send email, automatic review fixed

--- server/src/jobs/review.job.ts
+++ server/src/jobs/review.job.ts
@@ -1,15 +1,10 @@
-import { EmailTemplate } from '@/configs/email-template';
-import { env } from '@/configs/env';
 import { DocumentService } from '@/services/document.service';
 import { DocumentReviewService } from '@/services/documentreview.service';
-import { EmailService } from '@/services/email.service';
 import { logger } from '@/utils/logger';
-import { getNextReviewDate } from '@/utils/review';
-import { addDays, endOfDay, startOfDay } from 'date-fns';
+import { getNextReviewDate, shouldNotifyReview } from '@/utils/review';
 
 const docService = new DocumentService();
 const reviewService = new DocumentReviewService();
-const emailService = new EmailService();
 
 export async function generateDocumentReviewsJob() {
     const now = new Date();
@@ -69,62 +64,42 @@ export async function generateDocumentReviewsJob() {
     logger.info(`[REVIEW] Created ${count} reviews`);
 }
 
-const NOTIFY_BEFORE_DAYS = 2;
-
 export async function notifyReviewersJob() {
     logger.info('[NOTIFY] Starting notifyReviewersJob');
 
     try {
         const now = new Date();
-        const targetDateStart = startOfDay(addDays(now, NOTIFY_BEFORE_DAYS));
-        const targetDateEnd = endOfDay(addDays(now, NOTIFY_BEFORE_DAYS));
 
-        console.log(targetDateStart, targetDateEnd);
+        // Fetch all active reviews
+        const reviews = await reviewService.getActiveReviews();
 
-        // Find reviews that are approaching their reviewDate
-        const upcomingReviews = await reviewService.getUpcomingReviews({
-            targetDateStart,
-            targetDateEnd,
-        });
-
-        if (!upcomingReviews.length) {
-            logger.info('[NOTIFY] No upcoming reviews to notify');
+        if (!reviews.length) {
+            logger.info('[NOTIFY] No active reviews found');
             return;
         }
 
-        for (const review of upcomingReviews) {
-            const { reviewer, document, reviewDate } = review;
+        let notifiedCount = 0;
 
-            // Example: send email
-            const template = await EmailTemplate.reviewReminder({
-                document: {
-                    title: document.title,
-                    description: document.description,
-                    status: document.status,
-                },
-                dueDate: review.dueDate?.toDateString() || '',
-                reviewer: { name: reviewer.name },
-                year: new Date().getFullYear().toString(),
-                viewDocLink: `${env.CORS_ORIGIN}/documents/view/${document.id}`,
-                reviewLink: `${env.CORS_ORIGIN}/review-approval/${review.id}`,
-                headerDescription: 'Automated email',
-                orgName: env.ORG_NAME,
+        // Iterate and notify when needed
+        for (const review of reviews) {
+            const { document, dueDate, notifiedAt, isNotified } = review;
+
+            if (!dueDate || isNotified) continue;
+
+            const shouldNotify = shouldNotifyReview({
+                frequency: document.reviewFrequency!,
+                dueDate,
+                lastNotifiedAt: notifiedAt,
+                now,
             });
 
-            await emailService.sendMail({
-                subject: 'ISMS Solumada - Review Reminder',
-                to: reviewer.email,
-                html: template,
-            });
+            if (!shouldNotify) continue;
 
-            console.log('========== Send email to', reviewer.email);
-
-            logger.info(
-                `[NOTIFY] Reviewer ${reviewer.name} for document "${document.title}" due on ${reviewDate?.toDateString()}`,
-            );
+            await reviewService.sendReviewNotification(review);
+            notifiedCount++;
         }
 
-        logger.info(`[NOTIFY] Notified ${upcomingReviews.length} reviewers`);
+        logger.info(`[NOTIFY] Completed. ${notifiedCount} reviewers notified.`);
     } catch (err) {
         logger.error({ err }, '[NOTIFY] notifyReviewersJob failed');
     }
