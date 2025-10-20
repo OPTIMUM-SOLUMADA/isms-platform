ccecf027645311d1d56acf6f0cb27e348a000861
njatoty <njatotianafiononana@gmail.com>
2025-10-20 12:10:25 +0300

    refactor: move templates from src to server

--- server/src/configs/path.ts
+++ server/src/configs/path.ts
@@ -1,3 +1,4 @@
 import path from 'path';
 
 export const GOOGLE_DRIVE_JSON_PATH = path.join(process.cwd(), 'googledrive.json');
+export const TEMPLATE_DIR_PATH = path.join(process.cwd(), 'templates');
