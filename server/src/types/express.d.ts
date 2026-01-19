325c21c11f97a83875f38c1484141888dc360170
yunisuthn <yunis.thn@gmail.com>
2026-01-19 10:24:05 +0300

    update tsconfig

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,20 @@
+import { RoleType } from '@/types/roles';
+import { AuditStatus, User } from '@prisma/client';
+
+interface LogPayload {
+  event: AuditEventType;
+  details: Record<string, any>;
+  targets: AuditTarget[];
+  status?: AuditStatus;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: User;
+      log: (data: LogPayload) => Promise<void>;
+    }
+  }
+}
+
+export {};
