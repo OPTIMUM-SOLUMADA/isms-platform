9fc436386e73da73705a733151ca6ad4dc1306ce
yunisuthn <yunis.thn@gmail.com>
2026-01-30 10:04:47 +0300

    update google config

--- server/src/configs/google.config.ts
+++ server/src/configs/google.config.ts
@@ -6,10 +6,16 @@ export class GoogleAuthConfig {
     private oauth2Client: any;
 
     private constructor() {
+        if (!env.GOOGLE_CLIENT_ID || !env.GOOGLE_CLIENT_SECRET || !env.GOOGLE_REFRESH_TOKEN) {
+            throw new Error('Missing Google OAuth environment variables');
+        }
         this.oauth2Client = new google.auth.OAuth2({
             clientId: env.GOOGLE_CLIENT_ID!,
             clientSecret: env.GOOGLE_CLIENT_SECRET!,
             // redirectUri: env.GOOGLE_REDIRECT_URI!,
+        });
+        this.oauth2Client.setCredentials({
+            refresh_token: env.GOOGLE_REFRESH_TOKEN!,
         });
     }
 
